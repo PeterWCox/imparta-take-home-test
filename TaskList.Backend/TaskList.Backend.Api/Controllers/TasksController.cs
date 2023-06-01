89312f015a2d09a447c007cba352350a81889496
Peter Cox <p.cox@outlook.com>
2023-06-01 02:42:26 +0100

    Backend - Renaming project

---
+++ TaskList.Backend/TaskList.Backend.Api/Controllers/TasksController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskList.Backend.Api.Models;
+
+namespace TaskList.Backend.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskModel>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskModel>> GetTask(int id)
+        {
+            var toDoItemModel = await _context.Tasks.FindAsync(id);
+
+            if (toDoItemModel == null)
+            {
+                return NotFound();
+            }
+
+            return toDoItemModel;
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTask(int id, TaskModel toDoItemModel)
+        {
+            if (id != toDoItemModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDoItemModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoItemModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public async Task<ActionResult<TaskModel>> CreateTask(TaskModel toDoItemModel)
+        {
+            _context.Tasks.Add(toDoItemModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDoItemModel", new { id = toDoItemModel.Id }, toDoItemModel);
+        }
+
+        // DELETE: api/Task/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var toDoItemModel = await _context.Tasks.FindAsync(id);
+            if (toDoItemModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(toDoItemModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoItemModelExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TaskList.Backend/TaskList.Backend.Api/Models/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskList.Backend.Api.Authentication;
+
+namespace TaskList.Backend.Api.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TaskModel>(entity =>
+            {
+                entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+                entity.Property(e => e.IsDone)
+                .IsRequired()
+                .HasMaxLength(1);
+
+                entity.Property(e => e.Status)
+                 .IsRequired();
+
+            });
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
