e838c7635448b196def565835b9e3e93da313d43
Peter Cox <p.cox@outlook.com>
2023-06-06 08:46:00 +0100

    Backend - AuthenticationController - Remove unused endpoint

--- TaskList.Backend/TaskList.Backend.Api/Controllers/AuthenticationController.cs
+++ TaskList.Backend/TaskList.Backend.Api/Controllers/AuthenticationController.cs
@@ -166,49 +166,6 @@ public class AuthenticationController : ControllerBase
 
     }
 
-    [HttpPost]
-    [Route("register-admin")]
-    public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
-    {
-        var userExists = await _userManager.FindByNameAsync(model.Username);
-
-        if (userExists != null)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User already exists!" });
-        };
-
-        ApplicationUser user = new ApplicationUser()
-        {
-            Email = model.Email,
-            SecurityStamp = Guid.NewGuid().ToString(),
-            UserName = model.Username
-        };
-
-        var result = await _userManager.CreateAsync(user, model.Password);
-
-        if (!result.Succeeded)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
-        }
-
-        if (!await _roleManager.RoleExistsAsync(UserRoles.Admin))
-        {
-            await _roleManager.CreateAsync(new IdentityRole(UserRoles.Admin));
-        }
-
-        if (!await _roleManager.RoleExistsAsync(UserRoles.User))
-        {
-            await _roleManager.CreateAsync(new IdentityRole(UserRoles.User));
-        }
-
-        if (await _roleManager.RoleExistsAsync(UserRoles.Admin))
-        {
-            await _userManager.AddToRoleAsync(user, UserRoles.Admin);
-        }
-
-        return Ok(new Response { Status = "Success", Message = "User created successfully" });
-    }
-
     public async Task<JwtSecurityToken> GetToken(ApplicationUser user)
     {
         var userRoles = await _userManager.GetRolesAsync(user);
