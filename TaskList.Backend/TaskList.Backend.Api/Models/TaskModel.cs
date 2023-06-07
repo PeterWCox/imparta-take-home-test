ad0831ba2ce2ebb5f10d244e72d838a2eb0bf5fc
Peter Cox <p.cox@outlook.com>
2023-06-07 05:35:53 +0100

    Backend - Remove task validation temporarily

--- TaskList.Backend/TaskList.Backend.Api/Models/TaskModel.cs
+++ TaskList.Backend/TaskList.Backend.Api/Models/TaskModel.cs
@@ -8,14 +8,9 @@ public class TaskModel
     [Key]
     public int Id { get; set; }
 
-    [Required(ErrorMessage = "Title is required")]
-    [Column(TypeName = "nvarchar(100)")]
     public string Title { get; set; }
 
-    [Required(ErrorMessage = "IsDone is required")]
-    [Column(TypeName = "bit")]
     public bool IsDone { get; set; }
 
-    [Required(ErrorMessage = "Status is required")]
     public int Status { get; set; }
 }
